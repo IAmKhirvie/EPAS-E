1be5f8f5d62ae567a2dbfeb08589f4f31654dca4
khirvie <khirviecliffordbautista@gmail.com>
2026-04-21 22:37:58 +0800

    Fix register mobile scroll: edit correct file (public/css not resources/css), position static, single scroll

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // EPAS-E Service Worker
 // Update CACHE_VERSION when deploying new assets to bust the cache
-const CACHE_VERSION = '2026-04-21-v8';
+const CACHE_VERSION = '2026-04-21-v9';
 const CACHE_NAME = `epas-e-${CACHE_VERSION}`;
 const OFFLINE_URL = '/offline.html';
 
